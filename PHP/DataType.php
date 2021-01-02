a73dcc88fe35493454339285c194603a911528e8
siamshaeed <siamshaeed@gmail.com>
2021-01-02 18:29:04 +0600

    Data type in php

--- PHP/DataType.php
+++ PHP/DataType.php
@@ -1,3 +1,28 @@
 <?php
+   /* echo("PHP supports ten primitive types.");
+
+    Four scalar types:
+   1. Boolean
+   2. integer
+   3. float (floating-point number, aka double)
+   4. string
+
+    Four compound types:
+   1. array
+   2. object
+   3. callable
+   4. iterable
+
+    Two special types:
+   1. resource
+   2. NULL 
+   
+   */
     
+   //Boolean
+   $alive = true;
+   echo gettype($alive);
+   echo"<br/>";
+   //Integer
+   
 ?>
