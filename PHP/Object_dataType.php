31013eacd245ad0acd9a7c343e9b0890070e5f86
siamshaeed <siamshaeed@gmail.com>
2021-01-03 17:57:25 +0600

    Object in Data Type

--- PHP/Object_dataType.php
+++ PHP/Object_dataType.php
@@ -13,3 +13,41 @@ class greeting{
 $message = new greeting;
 var_dump($message);
 ?>
+
+
+<p>Example : 2</p>
+
+<?php
+/*
+ class student{
+function marks_calculation(){         
+echo "Display marks";
+}
+}
+$student1 = new student;
+$student1->marks_calculation(); 
+?>
+
+<p>Example : 3</p>
+<?php
+class foo{
+    function do_foo(){
+        echo "Doing foo."; 
+    }
+}
+$bar = new foo;
+$bar->do_foo();
+?>
+
+<p>Example : 4</p>
+<?php   
+     class bike{  
+          function model(){  
+               $model_name = "Royal Enfield";  
+               echo "Bike Model: " .$model_name;  
+             }  
+     }  
+     $obj = new bike();  
+     $obj -> model(); 
+*/
+?> 
