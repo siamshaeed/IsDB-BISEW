24fb9603ac309fccfa150b3f03e6084b2a9b7995
siamshaeed <siamshaeed@gmail.com>
2021-02-08 23:32:37 +0600

    Loop in php

--- PHP/Basic_PHP/loop.php
+++ PHP/Basic_PHP/loop.php
@@ -1,4 +1,5 @@
 <?php
+/*
 <p>Loops in PHP are used to execute the same block of code a specified number of times. PHP supports following four loop types.</p>
 <span>
     for − loops through a block of code a specified number of times.
@@ -6,7 +7,8 @@
     while − loops through a block of code if and as long as a specified condition is true.
 
     do...while − loops through a block of code once, and then repeats the loop as long as a special condition is true.
-
+    
     foreach − loops through a block of code for each element in an array.
 </span>
+*/
 ?>
