5c87ae4aada2fcc0146f77cef3d15403e085455c
siamshaeed <siamshaeed@gmail.com>
2021-03-25 16:01:42 +0600

    doWhileLoop

--- PHP/Basic_PHP/doWhileLoop.php
+++ PHP/Basic_PHP/doWhileLoop.php
@@ -1,3 +1,24 @@
 <?php 
+	/*The do...while statement will execute a block of code at least once - it then will repeat the loop as long as a condition is true.*/
 
+	//Example :1
+	$x=1;
+	do{
+		echo "The Number is : $x <br>";
+		$x++;
+	}while($x <= 5)
  ?>
+
+
+  <?php
+   //Example :2
+         $i = 0;
+         $num = 0;
+         
+         do {
+            $i++;
+         }
+         
+         while( $i < 10 );
+         echo ("Loop stopped at i = $i" );
+      ?>
