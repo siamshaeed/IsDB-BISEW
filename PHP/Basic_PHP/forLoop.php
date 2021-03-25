f27ffd3efd9955c5874e61536c0038ae5ec01e2b
siamshaeed <siamshaeed@gmail.com>
2021-03-25 14:56:20 +0600

    For Loop in Php

--- PHP/Basic_PHP/forLoop.php
+++ PHP/Basic_PHP/forLoop.php
@@ -1,6 +1,18 @@
 <?php 
+	//for − loops through a block of code a specified number of times
 	//Simple forLoop
+	
+	//Example 1:
 	for ($i=1; $i<=10; $i++){
 		echo "This is simple forloop <br>";
 	}
+
+	//Example 2:
+	$a=0;
+	$b=0;
+	for ($i=0; $i<4; $i++) { 
+		$a += 5;
+		$b += 10;
+	}
+	 echo ("At the end of the loop a = $a and b = $b" );
  ?>
