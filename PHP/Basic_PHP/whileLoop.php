4667a895935dd6aa59e686a9c5f646b92df7cb19
siamshaeed <siamshaeed@gmail.com>
2021-03-25 15:30:54 +0600

    while loop description

--- PHP/Basic_PHP/whileLoop.php
+++ PHP/Basic_PHP/whileLoop.php
@@ -1,4 +1,7 @@
 <?php
+/*The while statement will execute a block of code if and as long as a test expression is true.
+If the test expression is true then the code block will be executed. After the code has executed the test expression will again be evaluated and the loop will continue until the test expression is found to be false*/
+
 $x = 1;
 while ( $x <= 5) {
 	echo "The Number is : $x <br>";
