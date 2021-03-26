587b428a600b2d53b336d51f10a9566542382857
siamshaeed <siamshaeed@gmail.com>
2021-03-26 08:32:16 +0600

    array on php

--- PHP/Basic_PHP/Array.php
+++ PHP/Basic_PHP/Array.php
@@ -26,22 +26,53 @@
       var_dump($studentName);
       echo"<br> <br>";
 
+
       echo "<h2>Associative Array</h2>";
       /* First method to associate create array. */
       $salaries = array("mohammad" => 2000, "qadir" => 1000, "zara" => 500);
-      
+
       echo "Salary of mohammad is ". $salaries['mohammad'] . "<br />";
       echo "Salary of qadir is ".  $salaries['qadir']. "<br />";
       echo "Salary of zara is ".  $salaries['zara']. "<br />";
-      
+
       /* Second method to create array. */
       $salaries['mohammad'] = "high";
       $salaries['qadir'] = "medium";
       $salaries['zara'] = "low";
-      
+
       echo "Salary of mohammad is ". $salaries['mohammad'] . "<br />";
       echo "Salary of qadir is ".  $salaries['qadir']. "<br />";
       echo "Salary of zara is ".  $salaries['zara']. "<br />";
-      ?>
-      
+
+
+      echo "<h2>Multidimensional Array</h2>";
+      $marks = array( 
+        "mohammad" => array (
+         "physics" => 35,
+         "maths" => 30,   
+         "chemistry" => 39
+     ),
+
+        "qadir" => array (
+         "physics" => 30,
+         "maths" => 32,
+         "chemistry" => 29
+     ),
+
+        "zara" => array (
+         "physics" => 31,
+         "maths" => 22,
+         "chemistry" => 39
+     )
+    );
+
+      /* Accessing multi-dimensional array values */
+      echo "Marks for mohammad in physics : " ;
+      echo $marks['mohammad']['physics'] . "<br />"; 
+
+      echo "Marks for qadir in maths : ";
+      echo $marks['qadir']['maths'] . "<br />"; 
+
+      echo "Marks for zara in chemistry : " ;
+      echo $marks['zara']['chemistry'] . "<br />"; 
       ?>
