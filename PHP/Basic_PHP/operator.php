0a54e9b96bfc2cda879347f90bfa86985cfa7267
siamshaeed <siamshaeed@gmail.com>
2021-06-17 13:03:11 +0600

    logical operator

--- PHP/Basic_PHP/operator.php
+++ PHP/Basic_PHP/operator.php
@@ -1,9 +1,11 @@
 <?php
 echo "PHP operator:";
+/*
 	1. Arithmatic operator 
 		Bainary (+, -, *, /, %)
 		Unary (++, --, (-))
 	2. Assaignment operator (=, +=, -=, *=, /=, %=)
 	3. Conditional operator (>, >=, <, <=, ==, !=, ===, !===)
-
+	4. Logical operator (&&, ||, !)
+ */
 ?>
