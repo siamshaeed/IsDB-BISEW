04981308e3c015588f89418c9d4e050ccaaadda0
siamshaeed <siamshaeed@gmail.com>
2021-06-23 16:36:14 +0600

    frontend design

---
+++ PHP/mini_script/concatenateWordFromUser.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Concatenate word from user</title>
+</head>
+<body>
+	<form action="" method="POST">
+		<table>
+			<tr>
+				<td>First Name :</td>
+				<td><input type="text" name="firstName" placeholder="First Name"></td>
+			</tr>
+			<tr>
+				<td>Middle Name :</td>
+				<td><input type="text" name="middleName" placeholder="Middle Name"></td>
+			</tr>
+			<tr>
+				<td>last Name :</td>
+				<td><input type="text" name="lastName" placeholder="Last Name"></td>
+			</tr>
+			<tr>
+				<td>Full Name :</td>
+				<td><input type="text" placeholder="Full Name"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" name="btn" value="Submit"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
