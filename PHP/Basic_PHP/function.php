f9f661a41ef15bf81d74cd78f4ffc9f5b30f16c7
siamshaeed <siamshaeed@gmail.com>
2021-07-02 20:03:51 +0600

    exercise 1

---
+++ PHP/Basic_PHP/function.php
@@ -0,0 +1,11 @@
+<?php 
+	// Function Definition
+	function concation(){
+		$firstName = "Shaeed Al Hasan";
+		$lastName = "Siam";
+		$fullName = $firstName ." ". $lastName;
+		echo $fullName;
+	}
+	// Function Calling
+	concation();
+?>
