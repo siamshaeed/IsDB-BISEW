7987616d3538abd07f5a687d5a99580386049491
siamshaeed <siamshaeed@gmail.com>
2021-07-03 17:27:41 +0600

    function

--- PHP/Basic_PHP/function.php
+++ PHP/Basic_PHP/function.php
@@ -1,5 +1,5 @@
 <?php 
-/*
+
 	// Function Definition
 	function concation(){
 		$firstName = "Shaeed Al Hasan";
@@ -9,10 +9,10 @@
 	}
 	// Function Calling
 	concation();
-*/
-?>
 
-<?php 
+	?>
+
+	<?php 
 /*
 	function add($firstNumber, $secondNumber){
 		 $result = $firstNumber + $secondNumber;
@@ -21,4 +21,48 @@
 
 	echo add(10,2);
 */
- ?>
+	?>
+
+
+	<?php 
+	/*
+	$result = "";
+	if (isset($_POST['btn'])) {
+			// function definition
+		function myfullName($data){
+			$firstName = $data['firstName'];
+			$lastName = $data['lastName'];
+			$phone =$data['phoneNo'];
+			$fullName = $firstName ." ". $lastName ." ". $phone;
+			return $fullName;
+		}
+		// function calling
+		$result = myfullName($_POST);
+
+	}
+
+	echo $result
+
+	?>
+
+	<form action="" method="post">
+		<table>
+			<tr>
+				<td>First Name :</td>
+				<td><input type="text" name="firstName"></td>
+			</tr>
+			<tr>
+				<td>Last Name :</td>
+				<td><input type="text" name="lastName"></td>
+			</tr>
+			<tr>
+				<td>Phone :</td>
+				<td><input type="text" name="phoneNo"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" name="btn" value="Submit"></td>
+			</tr>
+		</table>
+	</form>
+	*/
